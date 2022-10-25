74b3ee5a01f397abe165ffd23a1195f0745c1333
Pedro_Camara <pedroocamaraoliveira@gmail.com>
2022-10-25 16:13:15 -0300

    start deploy

---
+++ script.bat
@@ -0,0 +1,17 @@
+echo "Criando as imagens..."
+
+docker build -t pedro-camara/pacifista-backend:1.0 backend/.
+docker build -t pedro-camara/pacifista-database:1.0 database/.
+
+echo "Realizando o push das imagens"
+
+docker push pedro-camara/pacifista-backend:1.0
+docker push pedro-camara/pacifista-database:1.0
+
+echo "Criando serviços no cluster k8's"
+
+kubectl apply -f ./services.yml
+
+echo "Criando o deploy no cluster k8's"
+
+kubectl aplly -f ./deploy.yml
